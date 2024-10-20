f692aa5a2d1903172a3350083b2548038edc6f96
sudip-dhakal <dhakalsudip900@gmail.com>
2024-10-20 13:47:37 +0545

    ..

--- src/Components/RulesField.jsx
+++ src/Components/RulesField.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const RulesField = ({ description }) => {
   return (
     <React.Fragment>
-      <div className="md:h-[3.5rem] sm:h-[2.5rem] bg-custom-cl-btn w-[80%] ml-auto mr-auto drop-shadow-custom-shadow rounded-[10px]">
+      <div className="md:h-[3.5rem] sm:h-[3rem] bg-custom-cl-btn w-[80%] ml-auto mr-auto drop-shadow-custom-shadow rounded-[10px]">
         <p className=" border-custom-cl-btn md:text-2xl sm:text-[1rem] items-center justify-center relative top-2  text-wrap">
           {description}
         </p>
